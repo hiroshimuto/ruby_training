54210b3a60152045b4da75d86939af3b04af1a9a
hiroshimuto <hiroshi.github@gmail.com>
2019-01-29 16:38:00 +0900

    training

--- training_5.rb
+++ training_5.rb
@@ -22,3 +22,43 @@ puts a #⇛{"japan"=>"円", "us"=>"dollar", "italy"=>"euro"}
 ### ハッシュ[キー] で値の取得可能
 a['japan']
 puts a['japan'] #⇛円
+
+## ハッシュを使った繰り返し処理
+
+### eachメソッドを使うと、キーと値の組み合わせを順に取り出せる
+a = {'japan' => 'yen', 'us' => 'dollar', 'italy' => 'euro'}
+a.each do |key, value|
+  puts "#{key}:#{value}"
+end
+#⇛ japan:yen
+#⇛ us:dollar
+#⇛ italy:euro
+
+## ハッシュの同値比較、要素数の取得、要素の削除
+## ハッシュの同値比較
+### == でハッシュ同士を比較できる
+a = {'x' => 1, 'y' => 2, 'z' => 3}
+b = {'x' => 1, 'y' => 2, 'z' => 3}
+c = {'y' => 2, 'z' => 3, 'x' => 1}
+d = {'x' => 10, 'y' => 20, 'z' => 30}
+
+# キーも値も同じであるためtrueが返る
+puts a == b #⇛ true
+# 順番は違うけれどキーも値も同じであるためtrueが返る
+puts a == c #⇛ false
+# キーと値が異なるためfalseが返る
+puts a == d #⇛ true
+
+## 要素数の取得
+### sizeメソッド(lengthメソッド)を使えばハッシュの要素の個数を調べられる
+puts a.size #⇛3
+puts a.length #⇛3
+
+## 要素の削除
+### deleteメソッドで指定したキーに対応する要素を削除できる
+a.delete('x')
+puts a #⇛{"y"=>2, "z"=>3}
+
+
+
+
